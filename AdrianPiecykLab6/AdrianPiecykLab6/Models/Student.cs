f50e03ef28dabcb5c101460fbb0ee7dfebfd8a56
Narvel2 <adrianpiecyk96@gmail.com>
2018-02-26 22:09:24 +0100

    VisualStudio project files

---
+++ AdrianPiecykLab6/AdrianPiecykLab6/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdrianPiecykLab6.Models
+{
+    public class Student
+    {
+        /// <summary>
+        /// Tworzenie modelu potrzebnego do zrobienia bazy w Entity Framework
+        /// jak widac połączenie one-to-many
+        /// </summary>
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public long Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string  DateOfBirth { get; set; }
+        [JsonIgnore]
+        public virtual List<Course> Courses { get; set; }
+      
+
+      
+
+
+    }
+}
